3ba8700dddca5ae83c90a39026f4315368b922e6
teemukissa <tremppu@hotmail.com>
2025-07-14 17:25:48 +0300

    feat: add `resolveCurrentLocale`

--- src/templates/localize.ts
+++ src/templates/localize.ts
@@ -1,10 +1,11 @@
-import { ParserContextTransformer } from '../parser-types';
+import { ParserContext, ParserContextTransformer } from '../parser-types';
 import { get } from '../parser-util';
 
 declare module '../expandable-types' {
   export interface GlobalContext {
     locales: string[];
     defaultLocale: string;
+    resolveCurrentLocale?: (context: ParserContext<unknown>) => string | Promise<string>;
   }
   export interface InstanceContext {
     currentLocale?: string;
@@ -13,8 +14,14 @@ declare module '../expandable-types' {
 
 export const localize: ParserContextTransformer = {
   when: ({ data, locales = [] }) => (data && typeof data === 'object' ? Object.keys(data).every((key) => locales.includes(key)) : false),
-  then: ({ data, defaultLocale, currentLocale = defaultLocale }) => {
-    if (typeof data !== 'object' || !data) return data;
-    return get(currentLocale, data);
+  then: async (context) => {
+    if (typeof context.data !== 'object' || !context.data) return context.data;
+    const { defaultLocale, currentLocale, resolveCurrentLocale } = context;
+    if (currentLocale) return get(currentLocale, context.data);
+    if (resolveCurrentLocale) {
+      const locale = await resolveCurrentLocale(context);
+      return get(locale, context.data);
+    }
+    return get(defaultLocale, context.data);
   },
 };
