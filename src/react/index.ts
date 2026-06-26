38d25b7750d6d205a87b88f38622b131d050039c
teemukissa <tremppu@hotmail.com>
2026-06-26 13:26:47 +0300

    feat(react): provide a revalidate function

--- src/react/index.ts
+++ src/react/index.ts
@@ -2,8 +2,10 @@ import { useState, useCallback, useRef, useEffect } from 'react';
 import { ParserFunction, ParserReturnValue } from '../parser-types';
 import { toHash } from '../to-hash';
 
-export const useParserValue = <R extends ParserFunction<object>>(data: any, parser: R) => {
+export const useParserValue = <R extends ParserFunction<object>>(incomingData: any, parser: R) => {
   type Result = ParserReturnValue<R>;
+
+  const _data = useRef(incomingData);
   const hasId = useRef<string | undefined>(undefined);
   const [result, setResult] = useState<Result | undefined>(undefined);
   const [loading, setLoading] = useState(true);
@@ -12,24 +14,36 @@ export const useParserValue = <R extends ParserFunction<object>>(data: any, pars
   const parse = useCallback(async () => {
     setLoading(true);
     try {
-      const result = await parser(data);
+      const result = await parser(_data.current);
       setResult(result as Result);
       setLoading(false);
       setError(undefined);
     } catch (error) {
-      console.error('Error parsing data', { error, data, parser });
+      console.error('Error parsing data', { error, data: _data.current, parser });
       setResult(undefined);
       setLoading(false);
       setError(error);
     }
-  }, [data, parser]);
+  }, [incomingData, parser]);
 
   useEffect(() => {
-    const hash = toHash(data);
+    _data.current = incomingData;
+    const hash = toHash(_data.current);
     if (hasId.current === hash) return;
     hasId.current = hash;
     parse();
-  }, [data, parse]);
+  }, [incomingData, parse]);
 
-  return { result, loading, error };
+  const revalidate = useCallback(
+    (updatedData = _data.current) => {
+      _data.current = updatedData;
+      hasId.current = undefined;
+      const hash = toHash(updatedData);
+      hasId.current = hash;
+      parse();
+    },
+    [parse],
+  );
+
+  return { result, loading, error, revalidate };
 };
